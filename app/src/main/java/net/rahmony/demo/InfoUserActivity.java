d301a3c25f5b34fba051eb53d3b4785c725569e3
RaHmOnY <aljabri1414@gmail.com>
2016-06-06 05:10:14 +0300

    up-date 1.0

---
+++ app/src/main/java/net/rahmony/demo/InfoUserActivity.java
@@ -0,0 +1,13 @@
+package net.rahmony.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InfoUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
